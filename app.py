2cb238af2ac32f39991fe5cb81b33d4527c196a3
favoratti <favoratti@gmail.com>
2025-05-18 17:00:28 -0700

    changing port

--- app.py
+++ app.py
@@ -154,4 +154,4 @@ app.mount("/", StaticFiles(directory="static", html=True), name="static")
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=8020, reload=True)
